71c5c991d8aea06d48572383ce1944fd6989aed0
matheusleobelo <bipoladogamer@gmail.com>
2023-10-04 02:12:46 -0300

    modulo-ts

---
+++ src/app/tab1/tab1.module.ts
@@ -0,0 +1,15 @@
+// tab1.module.ts
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { Tab1Page } from './tab1.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule,
+  ],
+  declarations: [Tab1Page],
+})
+export class Tab1PageModule {}
