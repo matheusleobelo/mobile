7f8304ba8bf71270937864481eee4ed157a0ea82
matheusleobelo <bipoladogamer@gmail.com>
2023-10-04 01:21:30 -0300

    logica faltando jogar valores na tela

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -1,3 +1,5 @@
+// global.service.ts
+
 import { Injectable } from '@angular/core';
 
 interface ObjetoCriado {
@@ -6,26 +8,50 @@ interface ObjetoCriado {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GlobalService {
 
   public selecionarOpcao: string = '';
   public objetosCriados: ObjetoCriado[] = [];
-  private contador: number = 0;
   public todosObjetos: ObjetoCriado[] = [];
+  private contadores: { [key: string]: number } = {};
 
   constructor() { }
   
   criarObjetoComContador(tipo: string): void {
-    this.contador++;
+    // Se o contador para o tipo ainda não existe, inicialize-o com 1
+    if (!this.contadores[tipo]) {
+      this.contadores[tipo] = 1;
+    }
+
+    // Concatena 'SP', 'SE' ou 'SG' antes do valor do contador, dependendo do tipo
+    const prefixo: string = this.obterPrefixoPorTipo(tipo);
     const novoObjeto: ObjetoCriado = {
-      valor: this.contador.toString(),
+      valor: `${prefixo}${this.contadores[tipo]}`,
       tipo: tipo,
     };
+
     this.objetosCriados.push(novoObjeto);
     this.todosObjetos.push(novoObjeto);
+
+    // Incrementa o contador para o tipo
+    this.contadores[tipo]++;
+
     this.mostrarTodosObjetosNoConsole();
+  }
+
+  private obterPrefixoPorTipo(tipo: string): string {
+    switch (tipo) {
+      case 'prioridade':
+        return 'SP';
+      case 'exames':
+        return 'SE';
+      case 'geral':
+        return 'SG';
+      default:
+        return '';
+    }
   }
 
   private mostrarTodosObjetosNoConsole(): void {
